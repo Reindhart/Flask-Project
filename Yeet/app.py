b81e3d84f1349593a2b4d428f91ef14174366250
Reindhart <japp.banjo@gmail.com>
2024-11-11 00:58:19 -0600

    Inicio - Pruebas

--- Yeet/app.py
+++ Yeet/app.py
@@ -8,5 +8,17 @@ app = Flask(__name__)
 def index():
     return render_template('inicio.html')
 
+@app.route('/carta')
+def carta():
+    return render_template('carta.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
 if __name__ == "__main__":
     app.run(debug=True)
